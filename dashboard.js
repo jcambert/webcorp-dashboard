2c5edbe7c9fdd40e3e74c7bda3d0e4df6a90c03d
Ambert Jean-Christophe <j.ambert@free.fr>
2015-08-27 20:46:27 +0200

    Update Dashboard Menus

--- dashboard.js
+++ dashboard.js
@@ -2,28 +2,28 @@
 
 	 
 var dashboard=angular.module('webcorp.dashboard',['webcorp.core']);
-dashboard.config(['angular.elementlogProvider',function(angular.elementlog){
-	angular.elementlog.debugEnabled(false);
+dashboard.config(['$logProvider',function($log){
+	$log.debugEnabled(false);
 	console.log('Application configurated');
 }]);
 
 dashboard.value('DefaultTemplateRoot','directives/templates/');
 
-dashboard.run(['angular.elementlog','configService','DefaultTemplateRoot',function(angular.elementlog,angular.elementconfig,angular.elementdefaultTplRoot){
+dashboard.run(['$log','configService','DefaultTemplateRoot',function($log,$config,$defaultTplRoot){
 	
-	angular.elementlog.log('Application running');
+	$log.log('Application running');
 	
-	angular.elementconfig.set('TemplateRoot',angular.elementdefaultTplRoot);
+	$config.set('TemplateRoot',$defaultTplRoot);
 }]);
 
-dashboard.controller('testctrl',['angular.elementscope','angular.elementlog','Menus',function(angular.elementscope,angular.elementlog,angular.elementmenus){
-	angular.elementlog.log('testctrl starting');
-	angular.elementmenus.add(null,'menu0',{label:'label0',route:'route_label0',icon:'tachometer'}).add(null,'menu1',{label:'label1',route:'route_label1',icon:'list-alt',isGroup:true}).add('menu1','menu2',{label:'label2',route:'route_label2'});
-	angular.elementlog.log('Menu');
-	angular.elementlog.log(angular.elementmenus.menus());
-	angular.elementlog.log('testctrl running');
+dashboard.controller('testctrl',['$scope','$log','Menus',function($scope,$log,$menus){
+	$log.log('testctrl starting');
+	$menus.add(null,'menu0',{label:'label0',route:'route_label0',icon:'tachometer'}).add(null,'menu1',{label:'label1',route:'route_label1',icon:'list-alt',isGroup:true}).add('menu1','menu2',{label:'label2',route:'route_label2'});
+	$log.log('Menu');
+	$log.log($menus.menus());
+	$log.log('testctrl running');
 	
-	angular.elementscope.menus=angular.elementmenus.menus();
+	$scope.menus=$menus.menus();
 }]);
 
 dashboard.directive('dashboard',[function(){
@@ -35,17 +35,17 @@ dashboard.directive('dashboard',[function(){
 	};
 }]);
 
-dashboard.directive('menus',['DefaultTemplateRoot','Menus','configService',function(angular.elementdefaultTplRoot,angular.elementmenus,angular.elementconfig){
+dashboard.directive('menus',['DefaultTemplateRoot','Menus','configService',function($defaultTplRoot,$menus,$config){
 	return {
 		restrict:'E',
 		replace:true,
 		templateUrl:function(elem,attrs){
-			return angular.elementconfig.get('TemplateRoot',angular.elementdefaultTplRoot)+'menus.tpl.html';
+			return $config.get('TemplateRoot',$defaultTplRoot)+'menus.tpl.html';
 		}
 	};
 }])
 
-dashboard.service('Roles',['angular.elementlog',function(angular.elementlog){
+dashboard.service('Roles',['$log',function($log){
 	var self={};
 	
 	var roles=['admin','user','guest'];
@@ -59,9 +59,9 @@ dashboard.service('Roles',['angular.elementlog',function(angular.elementlog){
 	
 	return self;
 }]);
-dashboard.service('Menus',['angular.elementlog','Roles',function(angular.elementlog,angular.elementroles){
+dashboard.service('Menus',['$log','Roles',function($log,$roles){
 	var self={};
-	self.roles=angular.elementroles.roles;
+	self.roles=$roles.roles;
 	var menus=[];
 	self.defaultRoles=['user'];
 	 /*
@@ -78,31 +78,31 @@ dashboard.service('Menus',['angular.elementlog','Roles',function(angular.element
 	 }]
 	 */
 	 var exist = function(id){
-	 	angular.elementlog.log('----- START EXIST ------');  
+	 	$log.log('----- START EXIST ------');  
 		var result = find(id);
-		angular.elementlog.log('Exist find:'+result);
-		angular.elementlog.log('----- END EXIST ------');
+		$log.log('Exist find:'+result);
+		$log.log('----- END EXIST ------');
 		 return result!='';
 	 }
 	 
 	 var findMenu = function(menus,id){
-			angular.elementlog.log('----- FIND INDEX ------');  
-		 	angular.elementlog.log('findIndex menus:'+id);angular.elementlog.log(menus);
+			$log.log('----- FIND INDEX ------');  
+		 	$log.log('findIndex menus:'+id);$log.log(menus);
 			 if(menus.length==0)return '';
 			 for(var i=0;i<menus.length;i++){
 			 	var menu=menus[i];
-				angular.elementlog.log('current menu:'+i);
-			 	angular.elementlog.log(menus[i]);
-			 	angular.elementlog.log('findIndex:'+menu.id+':'+id);
+				$log.log('current menu:'+i);
+			 	$log.log(menus[i]);
+			 	$log.log('findIndex:'+menu.id+':'+id);
 				var result= (menu.id==id);
-				angular.elementlog.log('result:'+result);
+				$log.log('result:'+result);
 				if(result) return menu;
 				if(Array.isArray(menu.items)){
-					 angular.elementlog.log('search in subitems');
+					 $log.log('search in subitems');
 				 	return findMenu(menu.items);
 				 }else{
-					 angular.elementlog.log(menu);
-					 angular.elementlog.log('menu has no subitems');
+					 $log.log(menu);
+					 $log.log('menu has no subitems');
 				 }
 				 
 			 }
@@ -110,12 +110,12 @@ dashboard.service('Menus',['angular.elementlog','Roles',function(angular.element
 	 }	 
 	 var find = function(id){
 			/* return _.filter(menus,function(menu){
-				angular.elementlog.log('findIndex:'+menu.id+':'+id);
+				$log.log('findIndex:'+menu.id+':'+id);
 			 	var result= (menu.id==id);	
-			 	angular.elementlog.log('result:'+result);
+			 	$log.log('result:'+result);
 			 	if(result) return true;
 			 	if(Array.isArray(menu.items)){
-					 angular.elementlog.log('search in subitems');
+					 $log.log('search in subitems');
 				 	return findIndex(menu.items);
 				 }	
 			 	return false;	
@@ -135,8 +135,8 @@ dashboard.service('Menus',['angular.elementlog','Roles',function(angular.element
 	
 	
 	self.add = function(groupId,itemId,options){
-		angular.elementlog.log('****************************************' );
-		angular.elementlog.log('Try Adding Menu:'+itemId + ' to ' + groupId);
+		$log.log('****************************************' );
+		$log.log('Try Adding Menu:'+itemId + ' to ' + groupId);
 		if(exist(itemId))return self;
 		
 		options = options || {};
@@ -162,8 +162,8 @@ dashboard.service('Menus',['angular.elementlog','Roles',function(angular.element
 
 		group.push( builder() );
 
-		angular.elementlog.log('Menus Add');
-		angular.elementlog.log(menus);
+		$log.log('Menus Add');
+		$log.log(menus);
 		return self;
 	};
 	
@@ -197,88 +197,90 @@ dashboard.service('Menus',['angular.elementlog','Roles',function(angular.element
 }]);
 	 
 angular.element(document).ready(function () {
+	alert('Document Ready Start');
 	//set main navigation aside
 	/* global employee_token */
 	function navSidebar(){
-		var sidebar = angular.element('#nav-sidebar');
+		alert('sidebar');
+		var sidebar = $('#nav-sidebar');
 		sidebar.off();
-		angular.element('.expanded').removeClass('expanded');
-		angular.element('.maintab').not('.active').closest('.submenu').hide();
+		$('.expanded').removeClass('expanded');
+		$('.maintab').not('.active').closest('.submenu').hide();
 		sidebar.on('click','.submenu_expand', function(){
-			var angular.elementnavId = angular.element(this).parent();
-			angular.element('.submenu-collapse').remove();
-			if(angular.element('.expanded').length ){
-				angular.element('.expanded > ul').slideUp('fast', function(){
-					var angular.elementtarget = angular.element('.expanded');
-					angular.elementtarget.removeClass('expanded');
-					angular.element(angular.elementnavId).not(angular.elementtarget).not('.active').addClass('expanded');
-					angular.element(angular.elementnavId).not(angular.elementtarget).not('.active').children('ul:first').hide().slideDown();
+			var $navId = $(this).parent();
+			$('.submenu-collapse').remove();
+			if($('.expanded').length ){
+				$('.expanded > ul').slideUp('fast', function(){
+					var $target = $('.expanded');
+					$target.removeClass('expanded');
+					$($navId).not($target).not('.active').addClass('expanded');
+					$($navId).not($target).not('.active').children('ul:first').hide().slideDown();
 				});
 			}
 			else {
-				angular.element(angular.elementnavId).not('.active').addClass('expanded');
-				angular.element(angular.elementnavId).not('.active').children('ul:first').hide().slideDown();
+				$($navId).not('.active').addClass('expanded');
+				$($navId).not('.active').children('ul:first').hide().slideDown();
 			}
 		});
 		//sidebar menu collapse
 		sidebar.find('.menu-collapse').on('click',function(){
-			angular.element(this).toggleClass('icon-rotate-90');
+			$(this).toggleClass('icon-rotate-90');
 
-			if (angular.element(this).hasClass('icon-rotate-90')) {
-				angular.element(this).css('margin-left', '5px');
-				angular.element('.page-head .page-title').css('padding-left', '70px');
-				angular.element('.page-head .breadcrumb').css('left', '70px');
-				angular.element('.page-head .page-subtitle').css('left', '70px');
+			if ($(this).hasClass('icon-rotate-90')) {
+				$(this).css('margin-left', '5px');
+				$('.page-head .page-title').css('padding-left', '70px');
+				$('.page-head .breadcrumb').css('left', '70px');
+				$('.page-head .page-subtitle').css('left', '70px');
 
 			} else {
-				angular.element(this).css('margin-left', '');
-				angular.element('.page-head .page-title').css('padding-left', '230px');
-				angular.element('.page-head .breadcrumb').css('left', '230px');
-				angular.element('.page-head .page-subtitle').css('left', '230px');
+				$(this).css('margin-left', '');
+				$('.page-head .page-title').css('padding-left', '230px');
+				$('.page-head .breadcrumb').css('left', '230px');
+				$('.page-head .page-subtitle').css('left', '230px');
 			}
 
-			angular.element('body').toggleClass('page-sidebar-closed');
-			angular.element('.expanded').removeClass('expanded');
-			angular.element.ajax({
+			$('body').toggleClass('page-sidebar-closed');
+			$('.expanded').removeClass('expanded');
+			/*$.ajax({
 				url: "index.php",
 				cache: false,
-				data: "token="+employee_token+'&ajax=1&action=toggleMenu&tab=AdminEmployees&collapse='+Number(angular.element('body').hasClass('page-sidebar-closed'))
-			});
+				data: "token="+employee_token+'&ajax=1&action=toggleMenu&tab=AdminEmployees&collapse='+Number($('body').hasClass('page-sidebar-closed'))
+			});*/
 		});
 
 		var menuCollapse = sidebar.find('.menu-collapse');
 
-		if (angular.element('body').hasClass('page-sidebar-closed')) {
+		if ($('body').hasClass('page-sidebar-closed')) {
 			menuCollapse.addClass('icon-rotate-90');
 			menuCollapse.css('margin-left', '5px');
-			angular.element('.page-head .page-title').css('padding-left', '70px');
-			angular.element('.page-head .breadcrumb').css('left', '70px');
-			angular.element('.page-head .page-subtitle').css('left', '70px');
+			$('.page-head .page-title').css('padding-left', '70px');
+			$('.page-head .breadcrumb').css('left', '70px');
+			$('.page-head .page-subtitle').css('left', '70px');
 
 		}
 	}
 
 	function navTopbarReset() {
 		ellipsed = [];
-		angular.element('#ellipsistab').remove();
-		angular.element('#nav-topbar ul.menu').find('li.maintab').each(function(){
-			angular.element(this).removeClass('hide');
+		$('#ellipsistab').remove();
+		$('#nav-topbar ul.menu').find('li.maintab').each(function(){
+			$(this).removeClass('hide');
 		});
 	}
 
 	//agregate out of bounds items from top menu into ellipsis dropdown
 	function navTopbarEllipsis() {
 		navTopbarReset();
-		angular.element('#nav-topbar ul.menu').find('li.maintab').each(function(){
-			if (angular.element(this).position().top > 0) {
-				ellipsed.push(angular.element(this).html());
-				angular.element(this).addClass('hide');
+		$('#nav-topbar ul.menu').find('li.maintab').each(function(){
+			if ($(this).position().top > 0) {
+				ellipsed.push($(this).html());
+				$(this).addClass('hide');
 			}
 		});
 		if (ellipsed.length > 0) {
-			angular.element('#nav-topbar ul.menu').append('<li id="ellipsistab" class="subtab has_submenu"><a href="#"><i class="icon-ellipsis-horizontal"></i></a><ul id="ellipsis_submenu" class="submenu"></ul></li>');
+			$('#nav-topbar ul.menu').append('<li id="ellipsistab" class="subtab has_submenu"><a href="#"><i class="icon-ellipsis-horizontal"></i></a><ul id="ellipsis_submenu" class="submenu"></ul></li>');
 			for (var i = 0; i < ellipsed.length; i++) {
-				angular.element('#ellipsis_submenu').append('<li class="subtab has_submenu">' + ellipsed[i] + '</li>');
+				$('#ellipsis_submenu').append('<li class="subtab has_submenu">' + ellipsed[i] + '</li>');
 			}
 		}
 	}
@@ -286,22 +288,22 @@ angular.element(document).ready(function () {
 	//set main navigation on top
 	function navTopbar() {
 		navTopbarReset();
-		angular.element('#nav-sidebar').attr('id','nav-topbar');
-		var topbar = angular.element('#nav-topbar');
+		$('#nav-sidebar').attr('id','nav-topbar');
+		var topbar = $('#nav-topbar');
 		topbar.off();
-		angular.element('span.submenu_expand').remove();
-		angular.element('.expanded').removeClass('expanded');
+		$('span.submenu_expand').remove();
+		$('.expanded').removeClass('expanded');
 		// expand elements with submenu
 		topbar.on('mouseenter', 'li.has_submenu', function(){
-			angular.element(this).addClass('expanded');
+			$(this).addClass('expanded');
 		});
 		topbar.on('mouseleave', 'li.has_submenu', function(){
-			angular.element(this).removeClass('expanded');
+			$(this).removeClass('expanded');
 		});
 		// hide element over menu width on load
 		navTopbarEllipsis();
 		//hide element over menu width on resize
-		angular.element(window).on('resize', function() {
+		$(window).on('resize', function() {
 			navTopbarEllipsis();
 		});
 	}
@@ -311,36 +313,36 @@ angular.element(document).ready(function () {
 		navTopbarReset();
 		// clean actual menu type
 		// get it in navigation whatever type it is
-		var navigation = angular.element('#nav-sidebar,#nav-topbar');
+		var navigation = $('#nav-sidebar,#nav-topbar');
 		navigation.find('.menu').hide();
 		var submenu = "";
 		// clean trigger
 		navigation.off().attr('id','nav-mobile');
-		angular.element('span.menu-collapse').off();
+		$('span.menu-collapse').off();
 		navigation.on('click.collapse','span.menu-collapse',function(){
-			if (angular.element(this).hasClass('expanded')){
-				angular.element(this).html('<i class="icon-align-justify"></i>');
+			if ($(this).hasClass('expanded')){
+				$(this).html('<i class="icon-align-justify"></i>');
 				navigation.find('ul.menu').hide();
 				navigation.removeClass('expanded');
-				angular.element(this).removeClass('expanded');
+				$(this).removeClass('expanded');
 				//remove submenu when closing nav
-				angular.element('#nav-mobile-submenu').remove();
+				$('#nav-mobile-submenu').remove();
 			}
 			else {
-				angular.element(this).html('<i class="icon-remove"></i>');
+				$(this).html('<i class="icon-remove"></i>');
 				navigation.find('ul.menu').removeClass('menu-close').show();
 				navigation.addClass('expanded');
-				angular.element(this).addClass('expanded');
+				$(this).addClass('expanded');
 			}
 		});
 		//get click for item which has submenu
 		navigation.on('click.submenu','.maintab.has_submenu a.title', function(e){
 			e.preventDefault();
 			navigation.find('.menu').addClass('menu-close');
-			angular.element('#nav-mobile-submenu').remove();
+			$('#nav-mobile-submenu').remove();
 			//create submenu
-			submenu = angular.element('<ul id="nav-mobile-submenu" class="menu"><li><a href="#" id="nav-mobile-submenu-back"><i class="icon-arrow-left"></i>'+ angular.element(this).html() +'</a></li></ul>');
-			submenu.append(angular.element(this).closest('.maintab').find('.submenu').html());
+			submenu = $('<ul id="nav-mobile-submenu" class="menu"><li><a href="#" id="nav-mobile-submenu-back"><i class="icon-arrow-left"></i>'+ $(this).html() +'</a></li></ul>');
+			submenu.append($(this).closest('.maintab').find('.submenu').html());
 			//show submenu
 			navigation.append(submenu);
 			submenu.show();
@@ -354,14 +356,14 @@ angular.element(document).ready(function () {
 
 	//unset mobile nav
 	function removeMobileNav(){
-		var navigation = angular.element('#nav-mobile');
-		angular.element('#nav-mobile-submenu').remove();
-		angular.element('span.menu-collapse').html('<i class="icon-align-justify"></i>');
+		var navigation = $('#nav-mobile');
+		$('#nav-mobile-submenu').remove();
+		$('span.menu-collapse').html('<i class="icon-align-justify"></i>');
 		navigation.off();
-		if (angular.element('body').hasClass('page-sidebar')){
+		if ($('body').hasClass('page-sidebar')){
 			navigation.attr('id',"nav-sidebar");
 			navSidebar();
-		} else if (angular.element('body').hasClass('page-topbar')){
+		} else if ($('body').hasClass('page-topbar')){
 			navigation.attr('id',"nav-topbar");
 			navTopbar();
 		}
@@ -370,10 +372,10 @@ angular.element(document).ready(function () {
 
 	//init main navigation
 	function initNav(){
-		if (angular.element('body').hasClass('page-sidebar')){
+		if ($('body').hasClass('page-sidebar')){
 			navSidebar();
 		}
-		else if (angular.element('body').hasClass('page-topbar')) {
+		else if ($('body').hasClass('page-topbar')) {
 			navTopbar();
 		}
 	}
@@ -385,25 +387,25 @@ angular.element(document).ready(function () {
 
 	// prevent mouseout + direct path to submenu on sidebar uncollapsed navigation + avoid out of bounds
 	var closingMenu, openingMenu;
-	angular.element('li.maintab.has_submenu').hover(function() {
-		var submenu = angular.element(this);
+	$('li.maintab.has_submenu').hover(function() {
+		var submenu = $(this);
 		if (submenu.is('.active') && submenu.children('ul.submenu').is(':visible')) {
 			return;
 		}
 		clearTimeout(openingMenu);
 		clearTimeout(closingMenu);
 		openingMenu = setTimeout(function(){
-			angular.element('li.maintab').removeClass('hover');
-			angular.element('ul.submenu.outOfBounds').removeClass('outOfBounds').css('top',0);
+			$('li.maintab').removeClass('hover');
+			$('ul.submenu.outOfBounds').removeClass('outOfBounds').css('top',0);
 			submenu.addClass('hover');
-			var h = angular.element( window ).height();
+			var h = $( window ).height();
 			var x = submenu.find('.submenu li').last().offset();
 			var l = x.top + submenu.find('.submenu li').last().height();
 			var f = 25;
-			if (angular.element('#footer').is(':visible')){
-				f = angular.element('#footer').height() + f;
+			if ($('#footer').is(':visible')){
+				f = $('#footer').height() + f;
 			}
-			var s = angular.element(document).scrollTop();
+			var s = $(document).scrollTop();
 			var position = h - l - f + s;
 			var out = false;
 			if ( position < 0) {
@@ -412,32 +414,34 @@ angular.element(document).ready(function () {
 			}
 		},50);
 	}, function() {
-		var submenu = angular.element(this);
+		var submenu = $(this);
 		closingMenu = setTimeout(function(){
 			submenu.removeClass('hover');
 		},250);
 	});
 
-	angular.element('ul.submenu').on('mouseenter', function(){
+	$('ul.submenu').on('mouseenter', function(){
 		clearTimeout(openingMenu);
 	});
 	
 	//show footer when reach bottom
 	function animateFooter(){
-		if(angular.element(window).scrollTop() + angular.element(window).height() === angular.element(document).height()) {
-			angular.element('#footer:hidden').removeClass('hide');
+		if($(window).scrollTop() + $(window).height() === $(document).height()) {
+			$('#footer:hidden').removeClass('hide');
 		} else {
-			angular.element('#footer').addClass('hide');
+			$('#footer').addClass('hide');
 		}
 	}
 
 	//scroll top
 	function animateGoTop() {
-		if (angular.element(window).scrollTop()) {
-			angular.element('#go-top:hidden').stop(true, true).fadeIn();
-			angular.element('#go-top:hidden').removeClass('hide');
+		if ($(window).scrollTop()) {
+			$('#go-top:hidden').stop(true, true).fadeIn();
+			$('#go-top:hidden').removeClass('hide');
 		} else {
-			angular.element('#go-top').stop(true, true).fadeOut();
+			$('#go-top').stop(true, true).fadeOut();
 		}
 	}
+	
+	alert('Document Ready End');
 });
