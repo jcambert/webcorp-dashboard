4a9782fe3eda0022b827b0325903df144f5bee55
jcambert <jc.ambert@free.fr>
2015-08-31 17:52:10 +0200

    Uppdate Dashboard Menus

--- menu.js
+++ menu.js
@@ -1,5 +1,7 @@
-
 'use strict';
+(function(angular,$,_){
+
+
 angular.module('webcorp.ui.menu',[])
 .service('Roles',['$log',function($log){
 	var self={};
@@ -51,41 +53,458 @@ angular.module('webcorp.ui.menu',[])
 	      return (pos ? separator : '') + letter.toLowerCase();
 	    });
 	  }
-	this.$get = ['$log','$document', '$interpolate','$compile','$menus',function($log,$document,$interpolate,$compile,$menus){
+	this.$get = ['$log','$document', '$interpolate','$compile','$templateRequest','$menus','configService',function($log,$document,$interpolate,$compile,$templateRequest,$menus,$config){
 		return function $menu(type,options){
 			options = angular.extend({}, defaultOptions, globalOptions, options);
-		 	var directiveName = snake_case(type);
-		 	var startSym = $interpolate.startSymbol();
-			var endSym = $interpolate.endSymbol();
+		 	//var directiveName = snake_case(type);
+		 	//var startSym = $interpolate.startSymbol();
+			//var endSym = $interpolate.endSymbol();
+			
+			function navSidebar(){
+				$log.log('init nav');
+				var sidebar = $('#nav-sidebar');
+				sidebar.off();
+				$('.expanded').removeClass('expanded');
+				$('.maintab').not('.active').closest('.submenu').hide();
+				sidebar.on('click','.submenu_expand', function(){
+					var $navId = $(this).parent();
+					$('.submenu-collapse').remove();
+					if($('.expanded').length ){
+						$('.expanded > ul').slideUp('fast', function(){
+							var $target = $('.expanded');
+							$target.removeClass('expanded');
+							$($navId).not($target).not('.active').addClass('expanded');
+							$($navId).not($target).not('.active').children('ul:first').hide().slideDown();
+						});
+					}
+					else {
+						$($navId).not('.active').addClass('expanded');
+						$($navId).not('.active').children('ul:first').hide().slideDown();
+					}
+				});
+				//sidebar menu collapse
+				sidebar.find('.menu-collapse').on('click',function(){
+					$(this).toggleClass('icon-rotate-90');
+		
+					if ($(this).hasClass('icon-rotate-90')) {
+						$(this).css('margin-left', '5px');
+						$('.page-head .page-title').css('padding-left', '70px');
+						$('.page-head .breadcrumb').css('left', '70px');
+						$('.page-head .page-subtitle').css('left', '70px');
+		
+					} else {
+						$(this).css('margin-left', '');
+						$('.page-head .page-title').css('padding-left', '230px');
+						$('.page-head .breadcrumb').css('left', '230px');
+						$('.page-head .page-subtitle').css('left', '230px');
+					}
+		
+					$('body').toggleClass('page-sidebar-closed');
+					$('.expanded').removeClass('expanded');
+					$.ajax({
+						url: "index.php",
+						cache: false,
+						data: "token="+employee_token+'&ajax=1&action=toggleMenu&tab=AdminEmployees&collapse='+Number($('body').hasClass('page-sidebar-closed'))
+					});
+				});
+		
+				var menuCollapse = sidebar.find('.menu-collapse');
+		
+				if ($('body').hasClass('page-sidebar-closed')) {
+					menuCollapse.addClass('icon-rotate-90');
+					menuCollapse.css('margin-left', '5px');
+					$('.page-head .page-title').css('padding-left', '70px');
+					$('.page-head .breadcrumb').css('left', '70px');
+					$('.page-head .page-subtitle').css('left', '70px');
+		
+				}
+			}
+		
+			function navTopbarReset() {
+				ellipsed = [];
+				$('#ellipsistab').remove();
+				$('#nav-topbar ul.menu').find('li.maintab').each(function(){
+					$(this).removeClass('hide');
+				});
+			}
+		
+			//agregate out of bounds items from top menu into ellipsis dropdown
+			function navTopbarEllipsis() {
+				navTopbarReset();
+				$('#nav-topbar ul.menu').find('li.maintab').each(function(){
+					if ($(this).position().top > 0) {
+						ellipsed.push($(this).html());
+						$(this).addClass('hide');
+					}
+				});
+				if (ellipsed.length > 0) {
+					$('#nav-topbar ul.menu').append('<li id="ellipsistab" class="subtab has_submenu"><a href="#"><i class="icon-ellipsis-horizontal"></i></a><ul id="ellipsis_submenu" class="submenu"></ul></li>');
+					for (var i = 0; i < ellipsed.length; i++) {
+						$('#ellipsis_submenu').append('<li class="subtab has_submenu">' + ellipsed[i] + '</li>');
+					}
+				}
+			}
+		
+			//set main navigation on top
+			function navTopbar() {
+				navTopbarReset();
+				$('#nav-sidebar').attr('id','nav-topbar');
+				var topbar = $('#nav-topbar');
+				topbar.off();
+				$('span.submenu_expand').remove();
+				$('.expanded').removeClass('expanded');
+				// expand elements with submenu
+				topbar.on('mouseenter', 'li.has_submenu', function(){
+					$(this).addClass('expanded');
+				});
+				topbar.on('mouseleave', 'li.has_submenu', function(){
+					$(this).removeClass('expanded');
+				});
+				// hide element over menu width on load
+				navTopbarEllipsis();
+				//hide element over menu width on resize
+				$(window).on('resize', function() {
+					navTopbarEllipsis();
+				});
+			}
+		
+			//set main navigation for mobile devices
+			function mobileNav() {
+				navTopbarReset();
+				// clean actual menu type
+				// get it in navigation whatever type it is
+				var navigation = $('#nav-sidebar,#nav-topbar');
+				navigation.find('.menu').hide();
+				var submenu = "";
+				// clean trigger
+				navigation.off().attr('id','nav-mobile');
+				$('span.menu-collapse').off();
+				navigation.on('click.collapse','span.menu-collapse',function(){
+					if ($(this).hasClass('expanded')){
+						$(this).html('<i class="icon-align-justify"></i>');
+						navigation.find('ul.menu').hide();
+						navigation.removeClass('expanded');
+						$(this).removeClass('expanded');
+						//remove submenu when closing nav
+						$('#nav-mobile-submenu').remove();
+					}
+					else {
+						$(this).html('<i class="icon-remove"></i>');
+						navigation.find('ul.menu').removeClass('menu-close').show();
+						navigation.addClass('expanded');
+						$(this).addClass('expanded');
+					}
+				});
+				//get click for item which has submenu
+				navigation.on('click.submenu','.maintab.has_submenu a.title', function(e){
+					e.preventDefault();
+					navigation.find('.menu').addClass('menu-close');
+					$('#nav-mobile-submenu').remove();
+					//create submenu
+					submenu = $('<ul id="nav-mobile-submenu" class="menu"><li><a href="#" id="nav-mobile-submenu-back"><i class="icon-arrow-left"></i>'+ $(this).html() +'</a></li></ul>');
+					submenu.append($(this).closest('.maintab').find('.submenu').html());
+					//show submenu
+					navigation.append(submenu);
+					submenu.show();
+				});
+				navigation.on('click.back','#nav-mobile-submenu-back',function(e){
+					e.preventDefault();
+					submenu.remove();
+					navigation.find('.menu').removeClass('menu-close').show();
+				});
+			}
+		
+			//unset mobile nav
+			function removeMobileNav(){
+				var navigation = $('#nav-mobile');
+				$('#nav-mobile-submenu').remove();
+				$('span.menu-collapse').html('<i class="icon-align-justify"></i>');
+				navigation.off();
+				if ($('body').hasClass('page-sidebar')){
+					navigation.attr('id',"nav-sidebar");
+					navSidebar();
+				} else if ($('body').hasClass('page-topbar')){
+					navigation.attr('id',"nav-topbar");
+					navTopbar();
+				}
+				navigation.find('.menu').show();
+			}
+		
+			//init main navigation
+			function initNav(){
+				if ($('body').hasClass('page-sidebar')){
+					navSidebar();
+				}
+				else if ($('body').hasClass('page-topbar')) {
+					navTopbar();
+				}
+			}
+		
+			//show footer when reach bottom
+			function animateFooter(){
+				if($(window).scrollTop() + $(window).height() === $(document).height()) {
+					$('#footer:hidden').removeClass('hide');
+				} else {
+					$('#footer').addClass('hide');
+				}
+			}
+		
+			//scroll top
+			function animateGoTop() {
+				if ($(window).scrollTop()) {
+					$('#go-top:hidden').stop(true, true).fadeIn();
+					$('#go-top:hidden').removeClass('hide');
+				} else {
+					$('#go-top').stop(true, true).fadeOut();
+				}
+			}
 			
 			
+			
+			function scroll_if_anchor(href) {
+				href = typeof(href) === "string" ? href : $(this).attr("href");
+				var fromTop = 120;
+				if(href.indexOf("#") === 0) {
+					var $target = $(href);
+					if($target.length) {
+						$('html, body').animate({ scrollTop: $target.offset().top - fromTop });
+						if(history && "pushState" in history) {
+							history.pushState({}, document.title, window.location.href + href);
+							return false;
+						}
+					}
+				}
+			}
+			var timer;
+			var ellipsed = [];
+			
+			// prevent mouseout + direct path to submenu on sidebar uncollapsed navigation + avoid out of bounds
+			var closingMenu, openingMenu;
+			
+			
+			function execute(){
+				
+				
+				initNav();
+				$log.log('Body');
+				$log.log($('body').html());
+				$('li.maintab.has_submenu').hover(function() {
+					$log.log('sub menu hover start');
+					var submenu = $(this);
+					if (submenu.is('.active') && submenu.children('ul.submenu').is(':visible')) {
+						return;
+					}
+					clearTimeout(openingMenu);
+					clearTimeout(closingMenu);
+					openingMenu = setTimeout(function(){
+						$log.log('opening menu');
+						$('li.maintab').removeClass('hover');
+						$('ul.submenu.outOfBounds').removeClass('outOfBounds').css('top',0);
+						submenu.addClass('hover');
+						var h = $( window ).height();
+						var x = submenu.find('.submenu li').last().offset();
+						var l = x.top + submenu.find('.submenu li').last().height();
+						var f = 25;
+						if ($('#footer').is(':visible')){
+							f = $('#footer').height() + f;
+						}
+						var s = $(document).scrollTop();
+						var position = h - l - f + s;
+						var out = false;
+						if ( position < 0) {
+							out = true;
+							submenu.find('.submenu').addClass('outOfBounds').css('top', position);
+						}
+					},50);
+					$log.log('sub menu hover end');
+				}, function() {
+					var submenu = $(this);
+					closingMenu = setTimeout(function(){
+						submenu.removeClass('hover');
+					},250);
+				});
+			
+				$('ul.submenu').on('mouseenter', function(){
+					clearTimeout(openingMenu);
+				});
+			
+				//media queries - depends of enquire.js
+				/*global enquire*/
+				enquire.register('screen and (max-width: 1200px)', {
+					match : function() {
+						if( $('#main').hasClass('helpOpen')) {
+							$('.toolbarBox a.btn-help').trigger('click');
+						}
+					},
+					unmatch : function() {
+			
+					}
+				});
+				enquire.register('screen and (max-width: 768px)', {
+					match : function() {
+			
+						$('body.page-sidebar').addClass('page-sidebar-closed');
+					},
+					unmatch : function() {
+						$('body.page-sidebar').removeClass('page-sidebar-closed');
+					}
+				});
+				enquire.register('screen and (max-width: 480px)', {
+					match : function() {
+						$('body').addClass('mobile-nav');
+						mobileNav();
+					},
+					unmatch : function() {
+						$('body').removeClass('mobile-nav');
+						removeMobileNav();
+					}
+				});
+			
+				//bootstrap components init
+				$('.dropdown-toggle').dropdown();
+				$('.label-tooltip, .help-tooltip').tooltip();
+				$('#error-modal').modal('show');
+			
+				//init footer
+				animateFooter();
+			
+				// go on top of the page
+				$('#go-top').on('click',function() {
+					$('html, body').animate({ scrollTop: 0 }, 'slow');
+					return false;
+				});
+			
+				
+				$(window).scroll(function() {
+					if(timer) {
+						window.clearTimeout(timer);
+					}
+					timer = window.setTimeout(function() {
+						animateGoTop();
+						animateFooter();
+					}, 100);
+				});
+			
+				// search with nav sidebar closed
+				$(document).on('click', '.page-sidebar-closed .searchtab' ,function() {
+					$(this).addClass('search-expanded');
+					$(this).find('#bo_query').focus();
+				});
+			
+				$('.page-sidebar-closed').click(function() {
+					$('.searchtab').removeClass('search-expanded');
+				});
+			
+				$('#header_search button').on('click', function(e){
+					e.stopPropagation();
+				});
+			
+				//erase button search input
+				if ($('#bo_query').val() !== '') {
+					$('.clear_search').removeClass('hide');
+				}
+			
+				$('.clear_search').on('click', function(e){
+					e.stopPropagation();
+					e.preventDefault();
+					var id = $(this).closest('form').attr('id');
+					$('#'+id+' #bo_query').val('').focus();
+					$('#'+id+' .clear_search').addClass('hide');
+				});
+				$('#bo_query').on('keydown', function(){
+					if ($('#bo_query').val() !== ''){
+						$('.clear_search').removeClass('hide');
+					}
+				});
+			
+				//search with nav sidebar opened
+				$('.page-sidebar').click(function() {
+					$('#header_search .form-group').removeClass('focus-search');
+				});
+			
+				$('#header_search #bo_query').on('click', function(e){
+					e.stopPropagation();
+					e.preventDefault();
+					if($('body').hasClass('mobile-nav')){
+						return false;
+					}
+					$('#header_search .form-group').addClass('focus-search');
+				});
+			
+				//select list for search type
+				$('#header_search_options').on('click','li a', function(e){
+					e.preventDefault();
+					$('#header_search_options .search-option').removeClass('active');
+					$(this).closest('li').addClass('active');
+					$('#bo_search_type').val($(this).data('value'));
+					$('#search_type_icon').removeAttr("class").addClass($(this).data('icon'));
+					$('#bo_query').attr("placeholder",$(this).data('placeholder'));
+					$('#bo_query').focus();
+				});
+			
+				// reset form
+				/* global header_confirm_reset, body_confirm_reset, left_button_confirm_reset, right_button_confirm_reset */
+				$(".reset_ready").click(function () {
+					var href = $(this).attr('href');
+					confirm_modal( header_confirm_reset, body_confirm_reset, left_button_confirm_reset, right_button_confirm_reset,
+						function () {
+							window.location.href = href + '&keep_data=1';
+						},
+						function () {
+							window.location.href = href + '&keep_data=0';
+					});
+					return false;
+				});
+			
+				//scroll_if_anchor(window.location.hash);
+				$("body").on("click", "a.anchor", scroll_if_anchor);
+				$log.log('executed');
+			}
 			
 			return {
 				restrict:'E',
 				replace:true,
-				link:function link($scope,$element,attrs,menuCtrl){
-					var template=angular.element('<ul class="menu"></ul>');
-					_.forEach($menus.menus(),function(element,index) {
-						$log.log('for each menu')
-						template.append('<li class="maintab" id="maintab-'+element.id+'" data-submenu="'+index+'"><a href="#" ui-sref="" class="title"></a></li>');
-						var a=template.append();
-						if(element.icon)
-							template.find('a:last').append('<i class="fa fa-'+element.icon+'"></i>');
-						template.find('a:last').append('<span>'+element.label+'</span>');
-						if(element.items){
-							var tpl1=angular.element('<ul class="submenu" style="top:0px;"></ul>');
-							_.forEach(element.items,function(item){
-								tpl1.append('<li id='+ item .id +'><a href="#" ui-sref="">' +item.label + '</li>')
-							});
-							template.find('a:last').append(tpl1);
-						}
-					}, this);
-					$log.log('link menu');
-					var linker = $compile(template);
-					$element.append(linker($scope));
-					$log.log(linker($scope));
+				templateUrl:function(elem,attrs){
 					
+						return $config.get('TemplateRoot','directives/templates/')+'menus.tpl.html';
+					},
+				scope:{
+					menus:'='
+				},
+				controller:function($log,$scope,$document){
+						angular.element($document).ready(function (){
+							
+							$log.log('doc ready');
+							//execute();
+						});
+				},
+				link:function($scope,$element,attrs,menuCtrl){
+					execute();
 				}
+				/*link:function($scope,$element,attrs,menuCtrl){
+					$templateRequest($config.get('TemplateRoot','directives/templates/')+'menus.tpl.html').then(function(template){
+						$log.log($scope.menus);
+						
+						$log.log(template);
+						var linker = $compile(template);
+						var html=linker($scope,function(html_){
+							$log.log(html_);
+							$element.parent().append(html_);
+							
+							execute();
+							
+						});
+						
+						
+						
+					});
+					
+					
+					$log.log('linked');
+				}*/
+						
+						/*var template=angular.element('<ul class="menu"><li data-ng-repeat="menu in menus" class="maintab " ng-class="{has_submenu:menu.items}" data-submenu="{{$index}}"><a href="#" class="title" ng-if="menu.shouldRender()" ui-sref="{{menu.route}}"><i class="fa fa-{{menu.icon}}"></i><span>{{menu.label}}</span></a><ul data-ng-if="menu.items"  class="submenu" style="top: 0px;"><li data-ng-repeat="item in menu.items"><a href="#" class="title" ng-if="item.label.length>0 && item.shouldRender()" ui-sref="{{item.route}}">{{item.label}}</a></li></ul></li></ul>');*/
+
 			};
 		};	
 	}];
@@ -111,31 +530,31 @@ angular.module('webcorp.ui.menu',[])
 	 }]
 	 */
 	 var exist = function(id){
-	 	$log.log('----- START EXIST ------');  
+	 	//$log.log('----- START EXIST ------');  
 		var result = find(id);
-		$log.log('Exist find:'+result);
-		$log.log('----- END EXIST ------');
+		//$log.log('Exist find:'+result);
+		//$log.log('----- END EXIST ------');
 		 return result!='';
 	 }
 	 
 	 var findMenu = function(menus,id){
-			$log.log('----- FIND INDEX ------');  
-		 	$log.log('findIndex menus:'+id);$log.log(menus);
+			//$log.log('----- FIND INDEX ------');  
+		 	//$log.log('findIndex menus:'+id);$log.log(menus);
 			 if(menus.length==0)return '';
 			 for(var i=0;i<menus.length;i++){
 			 	var menu=menus[i];
-				$log.log('current menu:'+i);
-			 	$log.log(menus[i]);
-			 	$log.log('findIndex:'+menu.id+':'+id);
+				//$log.log('current menu:'+i);
+			 	//$log.log(menus[i]);
+			 	//$log.log('findIndex:'+menu.id+':'+id);
 				var result= (menu.id==id);
-				$log.log('result:'+result);
+				//$log.log('result:'+result);
 				if(result) return menu;
 				if(Array.isArray(menu.items)){
-					 $log.log('search in subitems');
+					// $log.log('search in subitems');
 				 	return findMenu(menu.items);
 				 }else{
-					 $log.log(menu);
-					 $log.log('menu has no subitems');
+					 //$log.log(menu);
+					 //$log.log('menu has no subitems');
 				 }
 				 
 			 }
@@ -172,8 +591,8 @@ angular.module('webcorp.ui.menu',[])
 	}
 	
 	self.add = function(groupId,itemId,options){
-		$log.log('****************************************' );
-		$log.log('Try Adding Menu:'+itemId + ' to ' + groupId);
+		//$log.log('****************************************' );
+		//$log.log('Try Adding Menu:'+itemId + ' to ' + groupId);
 		if(exist(itemId))return self;
 		
 		options = options || {};
@@ -183,8 +602,8 @@ angular.module('webcorp.ui.menu',[])
 
 		group.push( builder(itemId,options) );
 
-		$log.log('Menus Add');
-		$log.log(menus);
+		//$log.log('Menus Add');
+		//$log.log(menus);
 		return self;
 	};
 	
@@ -216,3 +635,4 @@ angular.module('webcorp.ui.menu',[])
 	
 	return self;
 }]);
+}(angular,$,_));
